e38579a0e33d3ccfdd792f89289a072572222dab
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-04-08 12:24:23 +0900

    :sparkles: Feat: add skills container
    add skills container for Skills.jsx

---
+++ src/components/Work.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import WorkImg from "../assets/workImg.png";
+// import realEstate from "../assets/realestate.jpg";
+
+function Work() {
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">These are my recent projects.</p>
+        </div>
+
+        <div
+          style={{ backgroundImage: `url(${WorkImg})` }}
+          className="grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
+          <div
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto
+          content-div"
+          >
+            {/* Hover 효과*/}
+            <div>
+              <span></span>
+
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
