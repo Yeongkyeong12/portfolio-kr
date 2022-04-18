38444c5408f74a666b79b863b549277ebc669f31
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-04-18 11:17:22 +0900

    Feat: add cookle project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import WorkImg from "../assets/workImg.png";
 import WorkImg2 from "../assets/workImg2.png";
-import WorkImg3 from "../assets/workImg3.png";
+import WorkImg4 from "../assets/workImg4.png";
+import WorkImg5 from "../assets/workImg5.png";
+import WorkImg6 from "../assets/workImg6.png";
 
 function Project() {
   return (
@@ -19,8 +21,37 @@ function Project() {
 
         {/* Container */}
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
           {/* project #1 */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg2})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto
+          content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React.js Challenge App
+              </span>
+              <div className="m-2">#친환경챌린지 어플 #팀프로젝트</div>
+              <div className="pt-10 text-center">
+                <a href="http://15.164.98.50/" target="_blank" rel="noreferrer">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Green-Challenge/Green-Challenge-Client"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Github
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* project #2 */}
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto
@@ -54,25 +85,29 @@ function Project() {
             </div>
           </div>
 
-          {/* project #2 */}
+          {/* project #3 */}
           <div
-            style={{ backgroundImage: `url(${WorkImg2})` }}
+            style={{ backgroundImage: `url(${WorkImg6})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto
           content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React.js Challenge App
+                React.js Cookle Website
               </span>
-              <div className="m-2">#친환경 챌린지 어플 #팀프로젝트</div>
+              <div className="m-2">#요리검색 #개인프로젝트 #진행중</div>
               <div className="pt-10 text-center">
-                <a href="http://15.164.98.50/" target="_blank" rel="noreferrer">
+                <a
+                  href="https://cookle-project.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a
-                  href="https://github.com/Green-Challenge/Green-Challenge-Client"
+                  href="https://github.com/Yeongkyeong12/cookle"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -84,20 +119,20 @@ function Project() {
             </div>
           </div>
 
-          {/* project #3 */}
+          {/* project #4 */}
           <div
-            style={{ backgroundImage: `url(${WorkImg3})` }}
+            style={{ backgroundImage: `url(${WorkImg4})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto
           content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Vue.js Todo App
+                React.js Movie App
               </span>
-              <div className="m-2">#투두리스트 #개인프로젝트</div>
+              <div className="m-2">#영화검색앱 #개인프로젝트</div>
               <div className="pt-8 text-center">
                 <a
-                  href="https://melodious-pithivier-94556a.netlify.app/"
+                  href="https://react-movie-world.netlify.app/"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -106,7 +141,7 @@ function Project() {
                   </button>
                 </a>
                 <a
-                  href="https://github.com/Yeongkyeong12/vue-todolist"
+                  href="https://github.com/Yeongkyeong12/react-movie-app"
                   target="_blank"
                   rel="noreferrer"
                 >
